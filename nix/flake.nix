0eaf6d4347811cd3325080e4bbc84543488826cb
teutat3s <10206665+teutat3s@users.noreply.github.com>
2023-08-07 13:09:11 +0200

    Add nix devshell (flake) for quick dependency installation
    Using the nix package manager https://nixos.org/download

---
+++ nix/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "virtual environments";
+
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+
+  outputs = inputs@{ self, flake-parts, devshell, nixpkgs }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        devshell.flakeModule
+      ];
+
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      perSystem = { pkgs, ... }: {
+        devshells.default = {
+          packages = with pkgs; [
+            gnumake
+            go
+          ];
+        };
+      };
+    };
+}
